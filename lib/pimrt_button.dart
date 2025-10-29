5873f0e614263663ad2a9829e815efbd55871e72
waleedmoh766-web <waleedmoh766@gmail.com>
2025-10-29 08:25:29 +0300

    Revert "test screene"

--- lib/pimrt_button.dart
+++ lib/pimrt_button.dart
@@ -16,11 +16,10 @@ class PimrtButton extends StatelessWidget {
         foregroundColor: Colors.white,
         textStyle: const TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
       ),
-
       onPressed: () {
         // Define button action here
       },
-      child: const Text('Pimrt Button '),
+      child: const Text('Pimrt Button bbbfjdbfm smms  fjj'),
     );
   }
 }
